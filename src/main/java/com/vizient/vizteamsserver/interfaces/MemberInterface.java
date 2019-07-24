77b4f1f49f05af50a04553e52b860f43ededc5cf
Zack Amis <zachariah.amis@vizientinc.com>
2019-07-24 08:19:21 -0500

    cleaned up imports

--- src/main/java/com/vizient/vizteamsserver/interfaces/MemberInterface.java
+++ src/main/java/com/vizient/vizteamsserver/interfaces/MemberInterface.java
@@ -1,10 +1,11 @@
 package com.vizient.vizteamsserver.interfaces;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.vizient.vizteamsserver.requests.MemberRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface MemberInterface {
     @PostMapping(path = "member")
